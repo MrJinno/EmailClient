f7ebbc3354fbd60335ee89c0096d34a0b2a1ae08
swoje imie <wojtczakbartek7@gmail.com>
2020-02-14 21:57:45 +0100

    FirstVersion

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+module EmailClient {
+    requires javafx.fxml;
+        requires javafx.controls;
+        requires javafx.graphics;
+        requires javafx.web;
+
+        opens com.test;
+        }
