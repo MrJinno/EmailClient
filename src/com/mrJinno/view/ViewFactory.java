c41fd178b1a16b165d38c7c6bdd1fe408805bad4
swoje imie <wojtczakbartek7@gmail.com>
2020-02-16 00:04:58 +0100

    Added Css Styles, Created Css enums

--- src/com/mrJinno/view/ViewFactory.java
+++ src/com/mrJinno/view/ViewFactory.java
@@ -11,41 +11,55 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class ViewFactory {
     private EmailManager emailManager;
-     private FXMLLoader fxmlLoader;
-    private Stage stage= new Stage();
+    private FXMLLoader fxmlLoader;
+    private Stage stage= new Stage(), tempStage;
+    private ArrayList<Stage> activeStages;
+    private ColorTheme colorTheme= ColorTheme.DEFAULT;
+
+    private FontSize fontSize=FontSize.MEDIUM;
 
     public ViewFactory(EmailManager emailManager) {
+        activeStages=new ArrayList<>();
         this.emailManager = emailManager;
     }
+
     public void showLoginWindow(){
         System.out.println("Show Login Window Called");
         initializeScene(new LoginWindowController(emailManager, this, "LoginWindow.fxml"),false);
     }
+
     public void showMainWindow(){
         System.out.println("Show Main Window Called");
         initializeScene(new MainWindowController(emailManager, this, "MainWindow.fxml"), false);
     }
+
     public void showOptionsWindow(){
         System.out.println("Options window called");
-        initializeScene(new OptionsWindowController(emailManager, this, "OptionsWindow.fxml"), false);
+        initializeScene(new OptionsWindowController(emailManager, this, "OptionsWindow.fxml"), true);
     }
+
     public void initializeScene(Controller controller, boolean createNewWindow){
         Parent parent= manageFxmlObject(controller);
         if (createNewWindow){
-        Stage tempStage=new Stage();
+            tempStage=new Stage();
+            activeStages.add(tempStage);
         setScene(tempStage, parent);
         }else {
             setScene( stage, parent);
+            activeStages.add(tempStage);
         }
     }
+
     public Parent manageFxmlObject(Controller controller){
         fxmlLoader= new FXMLLoader(getClass().getResource(controller.getFxmlFile()));
         fxmlLoader.setController(controller);
        return getFxmlRoot(fxmlLoader);
     }
+
     public Parent getFxmlRoot(FXMLLoader fxmlLoader){
         try {
             return fxmlLoader.load();
@@ -54,10 +68,37 @@ public class ViewFactory {
             return null;
         }
     }
+
+    public void closeStage(Stage stage){
+        stage.close();
+        activeStages.remove(stage);
+    }
+    public void updateStyles() {
+        for (Stage stage: activeStages){
+            Scene scene=stage.getScene();
+            scene.getStylesheets().clear();
+            scene.getStylesheets().add(getClass().getResource("").toExternalForm());
+        }
+    }
+
     public void setScene(Stage stage, Parent parent){
         Scene scene = new Scene(parent);
         stage.setScene(scene);
         stage.show();
     }
 
+    public ColorTheme getColorTheme() {
+        return colorTheme;
+    }
+
+    public void setColorTheme(ColorTheme colorTheme) {
+        this.colorTheme = colorTheme;
+    }
+
+    public FontSize getFontSize() {
+        return fontSize;
+    }
+    public void setFontSize(FontSize fontSize) {
+        this.fontSize = fontSize;
+    }
 }
