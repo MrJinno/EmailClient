bad1008d22c851ddbed8b940697bc85f5c7782ea
swoje imie <wojtczakbartek7@gmail.com>
2020-02-15 19:10:55 +0100

    Added ViewFactory and Email Manager

--- src/com/mrJinno/controller/BaseController.java
+++ src/com/mrJinno/controller/BaseController.java
@@ -13,4 +13,7 @@ private String fxmlName;
         this.viewFactory = viewFactory;
         this.fxmlName = fxmlName;
     }
+    public String getFxmlFile(){
+    return fxmlName;
+    }
 }
--- src/com/mrJinno/view/ViewFactory.java
+++ src/com/mrJinno/view/ViewFactory.java
@@ -1,6 +1,14 @@
 package com.mrJinno.view;
 
 import com.mrJinno.EmailManager;
+import com.mrJinno.controller.BaseController;
+import com.mrJinno.controller.LoginWindowController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ViewFactory {
     private EmailManager emailManager;
@@ -9,8 +17,24 @@ public class ViewFactory {
         this.emailManager = emailManager;
     }
     public void showLoginWindow(){
+        Parent parent= loadFxmlFileAndSetController();
+        Scene scene= new Scene(parent);
+        Stage stage= new Stage();
+        stage.setScene(scene);
+        stage.show();
+
 
     }
-
+    public Parent loadFxmlFileAndSetController(){
+        BaseController controller= new LoginWindowController(emailManager, this, "LoginWindow.fxml");
+        FXMLLoader fxmlLoader= new FXMLLoader(getClass().getResource(controller.getFxmlFile()));
+        fxmlLoader.setController(controller);
+        try {
+            return fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
 }
